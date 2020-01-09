4a5efe3071ef93dbe3ad207f180d0a1825fd886e
Aryan <aryan.arora180@gmail.com>
2020-01-09 23:21:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/arena2020/ui/SportsFragment.java
@@ -0,0 +1,20 @@
+package com.example.arena2020.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.arena2020.R;
+
+public class SportsFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_sports, container, false);
+        return root;
+    }
+}
